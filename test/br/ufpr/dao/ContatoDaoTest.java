4bad030b1f5e25ea49408b9a0fe5d4c1cca89760
danielkarling3 <danielantoniokarling3@gmail.com>
2016-04-04 14:46:39 -0300

    criada função getLista()

--- test/br/ufpr/dao/ContatoDaoTest.java
+++ test/br/ufpr/dao/ContatoDaoTest.java
@@ -3,8 +3,12 @@ package br.ufpr.dao;
 import static org.junit.Assert.*;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.junit.Test;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
 import br.ufpr.modelo.Contato;
 
@@ -19,6 +23,16 @@ public class ContatoDaoTest {
 					"Rua tom Jobim",
 					ZonedDateTime.now());
 		contatoDao.adiciona(contato);
+		
+		ContatoDao contatoDao2 = new ContatoDao();
+		ArrayList<Contato> resultado = contatoDao2.getLista();
+		for (Contato r : resultado) {
+			  System.out.println("Nome: " + r.getNome());
+			  System.out.println("Email: " + r.getEmail());
+			  System.out.println("Endereço: " + r.getEndereco());
+			  
+			}
+		
 		
 	}
 
